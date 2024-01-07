271ab5c962188c4027db077779e4e547cb652de7
Sudhir Kumar <sudhir.kumar@credflow.in>
2024-01-07 14:06:07 +0530

    input and output in pytohn3

--- outlab3/p2/strings.py
+++ outlab3/p2/strings.py
@@ -1,12 +1,10 @@
-#! /usr/bin/env python
-f = open('employees.txt', 'rU')
-lines = f.readlines()
-for line in lines:
-      words = line.split()
-      for word in words:
-            string = word.split(',')
-            for strings in string:
-                  if strings.isalpha():
-                      print(strings[3:] + strings[:3])       
+with open('employees.txt', 'r') as input_file:
+    lines = input_file.readlines()
 
-f.close
+with open('output.txt', 'w') as output_file:
+    for line in lines:
+        words = line.split()
+        for word in words:
+            if word.isalpha():
+                output_file.write(word[3:] + word[:3] + " ")
+        output_file.write('\n')
